d8a504a4a83a801f2690ee2b8e7679ae8453abe7
farahaulia115 <farahaulia115@gmail.com>
2024-05-04 16:52:34 +0700

    lengkapin

--- Main/Inventory.java
+++ Main/Inventory.java
@@ -7,10 +7,9 @@ import Plant.Plant;
 public class Inventory {
     private Plant[] inventory;
     private Deck deck;
-
+ 
     public Inventory() {
         this.inventory = new Plant[10];
-        ArrayList<Plant> deck;
         deck = new Deck();
 
         // plants[1] = create plant tanaman 1 dst
@@ -32,7 +31,34 @@ public class Inventory {
     }
 
     public void swapDeck (int x, int y){
-        
+        Plant temp = deck.getDeck().get(x);
+        deck.getDeck().set(x, deck.getDeck().get(y));
+        deck.getDeck().set(y, temp);
+    }
+
+    public void deletePlant (int index){
+        deck.getDeck().remove(index);
+    }
+
+    // public void deletePlant (Plant plant){
+    //     deck.getDeck().equals()
+    //     for (Plant p: deck.getDeck()){
+    //         if (deck.getDeck().equals(p))
+    //     }
+    // }
+
+    public void addPlantToDeck (int i){
+        deck.getDeck().add(inventory[i]);
+    }
+
+    public void addPlantToDeck (Plant plant){
+        deck.getDeck().add(plant);
+    }
+
+    public void showInventory (){
+        for (int i = 0; i < 10; i++){
+            System.out.println((i+1) + " ." + inventory[i].getName());
+        }
     }
 
 
