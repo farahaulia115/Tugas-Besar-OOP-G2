726c49e8f19d7d31b5710f69da1b4e8ab3192cd9
farahaulia115 <farahaulia115@gmail.com>
2024-05-04 16:34:15 +0700

    buat deck dan inventory

--- Main/Sun.java
+++ Main/Sun.java
@@ -1,5 +1,25 @@
 package Main;
 
 public class Sun {
-    
+    private static int amount;
+
+    public static int getAmount(){
+        return amount;
+    }
+
+    // buat exception
+    public static void decreaseSun (int sun){
+        amount -= sun;
+    }
+
+    public static void increaseeSun (int sun){
+        amount += sun;
+    }
+
+    public static void setSun (int sun){
+        amount = sun;
+    }
+
+    // buat generate sun tiap berapa second
+
 }
