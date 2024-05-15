7efb2f23128ffa06a2c16f1a3008e1b66e2ac329
louisferdyo <louisferdyo1028@gmail.com>
2024-05-16 01:53:18 +0700

    yes

--- src/Plant/Squash.java
+++ src/Plant/Squash.java
@@ -23,14 +23,10 @@ public class Squash implements Plant, Attack, SelfDestruct {
     @Override
     public void attack(Zombie zombie) {
         zombie.setHealth(zombie.getHealth() - getAttackDamage());
-        
+        selfDestruct(tile);   
     }
     
-    public void attack(Zombie zombie, Tile tile) {
-        attack(zombie);
-        selfDestruct(tile);
-    }
-    
+    @Override
     public void selfDestruct(Tile tile) {
         try {
             tile.gali();
@@ -39,72 +35,74 @@ public class Squash implements Plant, Attack, SelfDestruct {
         }
     }
 
+    @Override
     public int getTimePlantCreated() {
         return timeCreated;
     }
+    @Override
     public void setName(String name) {
         this.name = name;
     }
+    @Override
     public void setCost(int cost) {
         this.cost = cost;
     }
+    @Override
     public void setHealth(int health) {
         this.health = health;
     }
+    @Override
     public void setAttackDamage(int attackDamage) {
         this.attackDamage = attackDamage;
     }
+    @Override
     public void setRange(int range) {
         this.range = range;
     }
+    @Override
     public void setCooldown(int cooldown) {
         this.cooldown = cooldown;
     }
 
-    public void setJumpable(boolean jumpable) {
-        this.jumpable = jumpable;
-    }
-
+    @Override
     public String getName() {
         return name;
     }
 
+    @Override
     public int getCost() {
         return cost;
     }
 
+    @Override
     public int getHealth() {
         return health;
     }
 
+    @Override
     public int getAttackDamage() {
         return attackDamage;
     }
 
+    @Override
     public int getAttackSpeed() {
         return attackSpeed;
     }
 
+    @Override
     public int getRange() {
         return range;
     }
 
+    @Override
     public int getCooldown() {
         return cooldown;
     }
 
+    @Override
     public boolean isJumpable() {
         return jumpable;
     }
-
-    public void attack() {
-        
-    }
-
-    public void selfDestruct() {
-        
-    }
-
 
 }
 
