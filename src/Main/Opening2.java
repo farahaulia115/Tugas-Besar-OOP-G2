0b4adcdbcaf6eadfa293b2ba2014eddcd6ba95af
farahaulia115 <farahaulia115@gmail.com>
2024-05-17 23:00:57 +0700

    refactor: Improve ASCII art display in Opening.java

--- src/Main/Opening2.java
+++ src/Main/Opening2.java
@@ -3,26 +3,25 @@ package Main;
 /**
  * Opening
  */
-public class Opening {
+public class Opening2 {
     public void printMvL(){
         String green = "\u001B[32m";  // Kode ANSI untuk warna hijau
         String red = "\u001B[31m";    // Kode ANSI untuk warna merah
-        String blue = "\u001B[34m";   // Kode ANSI untuk warna biru
         String reset = "\u001B[0m";   // Kode ANSI untuk mereset warna
         String[] asciiArt = new String[]{
 
-            red + "    ███╗   ███╗██╗ ██████╗██╗  ██╗ █████╗ ███████╗██╗         " + reset,
-            red + "    ████╗ ████║██║██╔════╝██║  ██║██╔══██╗██╔════╝██║         " + reset,
-            red + "    ██╔████╔██║██║██║     ███████║███████║█████╗  ██║         " + reset,
-            red + "    ██║╚██╔╝██║██║██║     ██╔══██║██╔══██║██╔══╝  ██║         " + reset,
-            red + "    ██║ ╚═╝ ██║██║╚██████╗██║  ██║██║  ██║███████╗███████╗    " + reset,
-            red + "    ╚═╝     ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝    " + reset,
-            blue + "                    ██╗   ██╗███████╗                         " + reset,
-            blue + "                    ██║   ██║██╔════╝                         " + reset,
-            blue + "                    ██║   ██║███████╗                         " + reset,
-            blue + "                    ╚██╗ ██╔╝╚════██║                         " + reset,
-            blue + "                     ╚████╔╝ ███████║                         " + reset,
-            blue + "                      ╚═══╝  ╚══════╝                         " + reset,
+            green + "    ███╗   ███╗██╗ ██████╗██╗  ██╗ █████╗ ███████╗██╗         " + reset,
+            green + "    ████╗ ████║██║██╔════╝██║  ██║██╔══██╗██╔════╝██║         " + reset,
+            green + "    ██╔████╔██║██║██║     ███████║███████║█████╗  ██║         " + reset,
+            green + "    ██║╚██╔╝██║██║██║     ██╔══██║██╔══██║██╔══╝  ██║         " + reset,
+            green + "    ██║ ╚═╝ ██║██║╚██████╗██║  ██║██║  ██║███████╗███████╗    " + reset,
+            green + "    ╚═╝     ╚═╝╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝    " + reset,
+            red + "                    ██╗   ██╗███████╗                         " + reset,
+            red + "                    ██║   ██║██╔════╝                         " + reset,
+            red + "                    ██║   ██║███████╗                         " + reset,
+            red + "                    ╚██╗ ██╔╝╚════██║                         " + reset,
+            red + "                     ╚████╔╝ ███████║                         " + reset,
+            red + "                      ╚═══╝  ╚══════╝                         " + reset,
             green + "██╗      █████╗ ██╗      █████╗ ██████╗  █████╗ ███╗   ██╗    " + reset,
             green + "██║     ██╔══██╗██║     ██╔══██╗██╔══██╗██╔══██╗████╗  ██║    " + reset,
             green + "██║     ███████║██║     ███████║██████╔╝███████║██╔██╗ ██║    " + reset,
