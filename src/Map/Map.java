6b7e6cacc062908feae17895bc37281ca16fc5d1
farrrahhh <18222096@std.stei.itb.ac.id>
2024-05-18 16:44:50 +0700

    refactor: Remove zombie from map reset

--- src/Map/Map.java
+++ src/Map/Map.java
@@ -253,6 +253,7 @@ public class Map{
     }
 
     public void resetMap(){
+        Map.getMapInstance().removeZombieInMap();
         for (int i = 0; i<=5;i++){
             for (int j = 0; j<=10;j++){
                 mapdetail[i][j].getZombieList().clear();
