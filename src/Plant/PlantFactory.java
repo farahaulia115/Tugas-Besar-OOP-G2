0a2e0d5cd34e89f9e3c710b2defd7fb64688f474
louisferdyo <louisferdyo1028@gmail.com>
2024-05-19 15:22:30 +0700

    samain nama plant

--- src/Plant/PlantFactory.java
+++ src/Plant/PlantFactory.java
@@ -4,13 +4,13 @@ package Plant;
 public class PlantFactory {
 
     public Plant createPlant(String type){
-        if (type.equalsIgnoreCase("SunFlower")){
+        if (type.equalsIgnoreCase("Sunflower")){
             return new SunFlower();
         } 
-        else if (type.equalsIgnoreCase("TwinSunFlower")){
+        else if (type.equalsIgnoreCase("Twin Sunflower")){
             return new TwinSunFlower();
         }
-        else if (type.equalsIgnoreCase("TangleKelp")){
+        else if (type.equalsIgnoreCase("Tangle kelp")){
             return new TangleKelp();
         }
         else if (type.equalsIgnoreCase("Peashooter")){
@@ -19,19 +19,19 @@ public class PlantFactory {
         else if (type.equalsIgnoreCase("Repeater")){
             return new Repeater();
         }
-        else if (type.equalsIgnoreCase("SnowPea")){
+        else if (type.equalsIgnoreCase("Snow pea")){
             return new SnowPea();
         }
         else if (type.equalsIgnoreCase("Squash")){
             return new Squash();
         }
-        else if (type.equalsIgnoreCase("Tallnut")){
+        else if (type.equalsIgnoreCase("Tall nut")){
             return new Tallnut();
         }
         else if (type.equalsIgnoreCase("Lilypad")){
             return new Lilypad();
         }
-        else if (type.equalsIgnoreCase("Wallnut")){
+        else if (type.equalsIgnoreCase("Wall nut")){
             return new Wallnut();
         }
        
