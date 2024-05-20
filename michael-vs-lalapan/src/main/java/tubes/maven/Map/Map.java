4ad5b828c5ff191deeb188500a6269f193a1e630
farrrahhh <18222096@std.stei.itb.ac.id>
2024-05-20 12:10:19 +0700

    "update"

--- michael-vs-lalapan/src/main/java/tubes/maven/Map/Map.java
+++ michael-vs-lalapan/src/main/java/tubes/maven/Map/Map.java
@@ -80,7 +80,7 @@ public class Map{
             mapdetail[x][y].tanam(plant);
     }
 
-    public void renderMap(){
+    public void renderMap3(){
         for (int i = 0; i<=5;i++){
             for (int j = 1; j<=10;j++){
                 System.out.printf("[");
@@ -108,7 +108,84 @@ public class Map{
         System.out.println();
     }
 
-    public void renderMap3() {
+
+    public void renderMap() {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j <= 10; j++) {
+                System.out.printf("[");
+                if (mapdetail[i][j].isAdaTanaman() || mapdetail[i][j].getZombieList().size() > 0) {
+                    if (mapdetail[i][j].isAdaTanaman()) {
+                        if (mapdetail[i][j].getPlant() instanceof SunFlower) {
+                            System.out.printf("S:" + mapdetail[i][j].getPlant().getHealth() + " ");
+                        } 
+                        else if (mapdetail[i][j].getPlant() instanceof TwinSunFlower){
+                            System.out.printf("TS:" + mapdetail[i][j].getPlant().getHealth() + " ");
+                        }
+                        else if (mapdetail[i][j].getPlant() instanceof Peashooter){
+                            System.out.printf("P:" + mapdetail[i][j].getPlant().getHealth() + " ");
+                        }
+                        else if (mapdetail[i][j].getPlant() instanceof Repeater){
+                            System.out.printf("R:" + mapdetail[i][j].getPlant().getHealth() + " ");
+                        }
+                        else if (mapdetail[i][j].getPlant() instanceof SnowPea){
+                            System.out.printf("SP:" + mapdetail[i][j].getPlant().getHealth() + " ");
+                        }
+                        else if (mapdetail[i][j].getPlant() instanceof Squash){
+                            System.out.printf("Sq:" + mapdetail[i][j].getPlant().getHealth() + " ");
+                        }
+                        else if (mapdetail[i][j].getPlant() instanceof Tallnut){
+                            System.out.printf("T:" + mapdetail[i][j].getPlant().getHealth() + " ");
+                        }
+                        else if (mapdetail[i][j].getPlant() instanceof Lilypad){
+                            System.out.printf("L:" + mapdetail[i][j].getPlant().getHealth() + " ");
+                        }
+                        else if (mapdetail[i][j].getPlant() instanceof Wallnut){
+                            System.out.printf("W:" + mapdetail[i][j].getPlant().getHealth() + " ");
+                        }
+                        else if (mapdetail[i][j].getPlant() instanceof TangleKelp){
+                            System.out.printf("TK:" + mapdetail[i][j].getPlant().getHealth() + " ");
+                        }
+                    }
+                    if (mapdetail[i][j].getZombieList().size() > 0) {
+                        for (Zombie z : mapdetail[i][j].getZombieList()) {
+                            if (z instanceof CanJump && !((CanJump) z).alreadyJumped()) {
+                                System.out.printf("J");
+                            }
+                            if (z instanceof NormalZombie) {
+                                System.out.printf("Z:" + z.getHealth() + " ");
+                            } else if (z instanceof ConeheadZombie) {
+                                System.out.printf("CZ:" + z.getHealth() + " ");
+                            } else if (z instanceof BucketheadZombie) {
+                                System.out.printf("BZ:" + z.getHealth() + " ");
+                            } else if (z instanceof FootballZombie) {
+                                System.out.printf("FZ:" + z.getHealth() + " ");
+                            } else if (z instanceof PolevaultingZombie) {
+                                System.out.printf("PZ:" + z.getHealth() + " ");
+                            } else if (z instanceof ScreendoorZombie) {
+                                System.out.printf("SZ:" + z.getHealth() + " ");
+                            } else if (z instanceof WallnutZombie) {
+                                System.out.printf("WZ:" + z.getHealth() + " ");
+                            } else if (z instanceof DolphinriderZombie) {
+                                System.out.printf("DoZ:" + z.getHealth() + " ");
+                            } else if (z instanceof DuckytubeConeheadZombie) {
+                                System.out.printf("DCZ:" + z.getHealth() + " ");
+                            } else if (z instanceof DuckytubeZombie) {
+                                System.out.printf("DZ:" + z.getHealth() + " ");
+                            }
+                        }
+                    }
+                } else {
+                    System.out.printf(" ");
+                }
+                System.out.printf("]");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+    public void renderMap4() {
         for (int i = 0; i <= 5; i++) {
             for (int j = 0; j <= 10; j++) {
                 System.out.printf("[");
