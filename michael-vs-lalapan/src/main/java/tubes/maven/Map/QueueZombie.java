a2c4dc848805db3bb5830593414e08e42c9fce1a
wNSYA <ucokb037@gmail.com>
2024-05-24 07:50:42 +0700

    tesrun, hapus file ga perlu

--- michael-vs-lalapan/src/main/java/tubes/maven/Map/QueueZombie.java
+++
@@ -1,8 +0,0 @@
-package tubes.maven.Map;
-
-import tubes.maven.Zombie.Zombie;
-
-import java.util.concurrent.ConcurrentLinkedQueue;
-public class QueueZombie<T extends Zombie> extends ConcurrentLinkedQueue<T>{
-    
-}
